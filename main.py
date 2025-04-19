0fe0ebf417837325d5f439520f4b40c0d9aa8792
Ali Burak <aliburak9627@gmail.com>
2025-04-20 02:54:31 +0300

    ✅ CORS tüm kaynaklara açıldı (Access-Control-Allow-Origin)

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ security = HTTPBasic()
 # ✅ CORS Ayarı
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://neso-guncel.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
